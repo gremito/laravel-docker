dd05daca456d7f524556d00867699eee00db3895
gremito <grem.takuya0223@gmail.com>
2024-10-02 03:09:15 +0900

    remove octane and set php-fpm

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-set -e
-composer install
-php artisan octane:start --server=swoole --host=0.0.0.0 --port=9000
+
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+fi
+
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env $APP_ENV"
+    cp .env.example .env
+else
+    echo "env file exists."
+fi
+
+php artisan migrate
+php artisan optimize
+php artisan view:cache
+
+php-fpm -D
+nginx -g "daemon off;"
