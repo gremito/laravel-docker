8a7478a382bbc08f768ea4f68d02f7ec18d6c160
gremito <grem.takuya0223@gmail.com>
2024-10-03 22:37:26 +0900

    update entrypoint.sh

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -25,4 +25,4 @@ if [ -z "$APP_KEY" ]; then
   fi
 fi
 
-php artisan octane:start --server=swoole --host=0.0.0.0 --port=9000
+php -d variables_order=EGPCS artisan octane:start --server=roadrunner --host=0.0.0.0 --rpc-port=6001 --port=80
