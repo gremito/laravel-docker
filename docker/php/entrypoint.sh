567fccb58942511ee19cdcab450d9e772663402f
gremito <grem.takuya0223@gmail.com>
2024-10-30 15:19:47 +0900

    wip

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
 composer install --no-progress --no-interaction
-echo "OCTANE_WORKERS: ${OCTANE_WORKERS}" 
-echo ${OCTANE_WORKERS} 
-echo "OCTANE_MAX_REQUESTS: ${OCTANE_MAX_REQUESTS}" 
-echo ${OCTANE_MAX_REQUESTS} 
-php artisan octane:start --server=roadrunner --host=0.0.0.0 --rpc-port=6001 --port=80 --workers=${OCTANE_WORKERS} --max-requests=${OCTANE_MAX_REQUESTS}
+php artisan octane:start --server=roadrunner --host=0.0.0.0 --rpc-port=6001 --port=80
