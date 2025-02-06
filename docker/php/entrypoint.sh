82e11e8ffdf777ab85f0091c4d2a9b0d6963e615
gremito <grem.takuya0223@gmail.com>
2025-02-06 11:55:29 +0900

    fix ci run env

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -2,15 +2,24 @@
 set -e
 
 if [ ! -f "vendor/autoload.php" ]; then
+    echo "===== composer install --no-progress --no-interaction ====="
     composer install --no-progress --no-interaction
+fi
+if [ ! -d "./vendor" ]; then
+    echo "Error: vendor not found."
+    exit 1
 fi
 
 if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
+    echo "===== cp .env.example .env ====="
     cp .env.example .env
 else
     echo "env file exists."
 fi
+
+sed -i "s/^OCTANE_SERVER=.*/OCTANE_SERVER=roadrunner/" .env
+octane_server_type=$(grep '^OCTANE_SERVER=' .env | cut -d '=' -f2-)
+echo "octane_server_type: ${octane_server_type}"
 
 APP_KEY=$(grep '^APP_KEY=' .env | cut -d '=' -f2-)
 if [ -z "$APP_KEY" ]; then
@@ -25,4 +34,19 @@ if [ -z "$APP_KEY" ]; then
   fi
 fi
 
+if [ ! -f "rr" ]; then
+    echo "===== remove rr ====="
+    rm -fr ./rr
+fi
+
+echo "===== vendor/bin/rr get-binary ====="
+yes | ./vendor/bin/rr get-binary
+
+echo "===== php artisan migrate ====="
+php artisan migrate
+
+echo "===== php artisan db:seed ====="
+php artisan db:seed
+
+echo "===== php artisan octane:start --server=roadrunner --host=0.0.0.0 --rpc-port=6001 --port=80 --workers=${OCTANE_WORKERS} --max-requests=${OCTANE_MAX_REQUESTS} ====="
 php artisan octane:start --server=roadrunner --host=0.0.0.0 --rpc-port=6001 --port=80 --workers=${OCTANE_WORKERS} --max-requests=${OCTANE_MAX_REQUESTS}
