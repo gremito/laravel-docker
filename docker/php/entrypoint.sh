139becc578e33ebc0a3b9fa47d54f4136ed4e5b5
gremito <grem.takuya0223@gmail.com>
2025-02-13 15:53:26 +0900

    wip: update Dockerfile

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,14 +1,27 @@
-#!/bin/bash
+#!/bin/sh
+
 set -e
 
-if [ ! -f "vendor/autoload.php" ]; then
-    echo "===== composer install --no-progress --no-interaction ====="
-    composer install --no-progress --no-interaction
-fi
-if [ ! -d "./vendor" ]; then
-    echo "Error: vendor not found."
-    exit 1
-fi
+composer_install() {
+  echo "===== composer install ====="
+  composer install --no-progress --no-interaction
+}
+composer_update() {
+  echo "===== composer update ====="
+  composer update --no-dev --optimize-autoloader
+}
+reset_checksum() {
+  echo "===== md5sum composer.json composer.lock > composer-checksum ====="
+  md5sum composer.json composer.lock > composer-checksum
+}
+artisan_migration() {
+  echo "===== php artisan migrate ====="
+  php artisan migrate
+}
+artisan_seed() {
+  echo "===== php artisan db:seed ====="
+  php artisan db:seed
+}
 
 if [ ! -f ".env" ]; then
     echo "===== cp .env.example .env ====="
@@ -17,9 +30,9 @@ else
     echo "env file exists."
 fi
 
-sed -i "s/^OCTANE_SERVER=.*/OCTANE_SERVER=roadrunner/" .env
-octane_server_type=$(grep '^OCTANE_SERVER=' .env | cut -d '=' -f2-)
-echo "octane_server_type: ${octane_server_type}"
+# sed -i "s/^OCTANE_SERVER=.*/OCTANE_SERVER=roadrunner/" .env
+# octane_server_type=$(grep '^OCTANE_SERVER=' .env | cut -d '=' -f2-)
+# echo "octane_server_type: ${octane_server_type}"
 
 APP_KEY=$(grep '^APP_KEY=' .env | cut -d '=' -f2-)
 if [ -z "$APP_KEY" ]; then
@@ -34,19 +47,37 @@ if [ -z "$APP_KEY" ]; then
   fi
 fi
 
-if [ ! -f "rr" ]; then
-    echo "===== remove rr ====="
-    rm -fr ./rr
+if [ ! -f composer-checksum ]; then
+  # 初回セットアップ
+  composer_install
+  echo "===== rr get-binary ====="
+  yes | ./vendor/bin/rr get-binary
+  sudo chmod +x root:root ./rr
+  artisan_migration
+  artisan_seed
+  reset_checksum
+else
+  # composer.json と composer.lock のハッシュ値を比較
+  current_json_hash=$(md5sum composer.json | cut -d ' ' -f1)
+  current_lock_hash=$(md5sum composer.lock | cut -d ' ' -f1)
+  prev_json_hash=$(grep 'composer.json' composer-checksum | cut -d ' ' -f1)
+  prev_lock_hash=$(grep 'composer.lock' composer-checksum | cut -d ' ' -f1)
+  json_changed=$([ "$current_json_hash" != "$prev_json_hash" ] && echo true || echo false)
+  lock_changed=$([ "$current_lock_hash" != "$prev_lock_hash" ] && echo true || echo false)
+  if [ "$json_changed" = true ] && [ "$lock_changed" = true ]; then
+    # composer.jsonまたはcomposer.lockが変更されている場合は再セットアップ
+    composer_install
+    reset_checksum
+  elif [ "$json_changed" = true ]; then
+    # composer.jsonのみ変更がある場合はcomposer update
+    composer_update
+    reset_checksum
+  elif [ "$lock_changed" = true ]; then
+    # composer.lockのみ変更がある場合は例外
+    echo "Error: composer.lockの変更のみでcomposer.jsonの変更がありません。"
+    exit 1
+  fi
+  artisan_migration
 fi
 
-echo "===== vendor/bin/rr get-binary ====="
-yes | ./vendor/bin/rr get-binary
-
-echo "===== php artisan migrate ====="
-php artisan migrate
-
-echo "===== php artisan db:seed ====="
-php artisan db:seed
-
-echo "===== php artisan octane:start --server=roadrunner --host=0.0.0.0 --rpc-port=6001 --port=80 --workers=${OCTANE_WORKERS} --max-requests=${OCTANE_MAX_REQUESTS} ====="
-php artisan octane:start --server=roadrunner --host=0.0.0.0 --rpc-port=6001 --port=80 --workers=${OCTANE_WORKERS} --max-requests=${OCTANE_MAX_REQUESTS}
+exec "$@"
